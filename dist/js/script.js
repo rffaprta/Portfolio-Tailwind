0eb9c0565009c1d8f32f5706749440c6be463b46
rffaprta <reffatama@gmail.com>
2026-02-18 03:40:57 +0700

    OKE! BLOM Rapihin

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,11 +10,19 @@ window.onscroll = function () {
   }
 };
 
-// Hamburger
+// Hamburger Menu Toggle
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Klik di luar hamburger menu buat nutup menu (biar makin responsive)
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
